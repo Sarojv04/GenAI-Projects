642c7a330f5bfb3966818d25edbe93cbdee18a49
Sarojv04 <sarojv0407@gmail.com>
2026-08-04 18:32:29 -0400

    messages type1

--- Prompt_Eng_Application/messages.py
+++ Prompt_Eng_Application/messages.py
@@ -1,16 +1,24 @@
 from langchain_openai import ChatOpenAI
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
 from langchain_core.messages import SystemMessage, HumanMessage, AIMessage
 from dotenv import load_dotenv
 
+
 load_dotenv()
 
-model = ChatOpenAI()
+llm = HuggingFaceEndpoint(
+    repo_id="Qwen/Qwen2.5-72B-Instruct",
+    task="text-generation"
+)
 
-messages_hist = = [
+model = ChatHuggingFace(llm=llm)
 
-    SystemMessage(content = 'you are a helpfull assistance')
+#model = ChatOpenAI()
+
+messages_hist = [
+    SystemMessage(content = 'you are a helpfull assistance'),
     HumanMessage(content = 'Tell me about langchain')
-]
+    ]
 result = model.invoke(messages_hist)
 messages_hist.append(AIMessage(content='result.content'))
 print (messages_hist)
